2f19c8267a5f062ec9068ebd78843b9e5ecd6707
tycho garen <garen@tychoish.com>
2016-03-30 09:00:54 -0400

    refactor priority conversions

--- priority.go
+++ priority.go
@@ -32,27 +32,9 @@ func GetThresholdLevel() int {
 }
 
 func (self *Journaler) GetThresholdLevelString() string {
-	switch {
-	case self.thresholdLevel == 0:
-		return "emergency"
-	case self.thresholdLevel == 1:
-		return "alert"
-	case self.thresholdLevel == 2:
-		return "critical"
-	case self.thresholdLevel == 3:
-		return "error"
-	case self.thresholdLevel == 4:
-		return "warning"
-	case self.thresholdLevel == 5:
-		return "notice"
-	case self.thresholdLevel == 6:
-		return "info"
-	case self.thresholdLevel == 7:
-		return "debug"
-	default:
-		return ""
-	}
+	return priorityString(convertPriority(self.thresholdLevel, self.defaultLevel))
 }
+
 func GetThresholdLevelString() string {
 	return std.GetThresholdLevelString()
 }
@@ -68,52 +50,71 @@ func convertPriority(priority interface{}, fallback journal.Priority) journal.Pr
 	}
 }
 
-func convertPriorityInt(priority int, fallback journal.Priority) journal.Priority {
-	p := fallback
+func priorityString(priority journal.Priority) string {
+	switch {
+	case priority == journal.PriEmerg:
+		return "emergency"
+	case priority == journal.PriAlert:
+		return "alert"
+	case priority == journal.PriCrit:
+		return "critical"
+	case priority == journal.PriErr:
+		return "error"
+	case priority == journal.PriWarning:
+		return "warning"
+	case priority == journal.PriNotice:
+		return "notice"
+	case priority == journal.PriInfo:
+		return "info"
+	case priority == journal.PriDebug:
+		return "debug"
+	default:
+		return ""
+	}
+}
 
+func convertPriorityInt(priority int, fallback journal.Priority) journal.Priority {
 	switch {
 	case priority == 0:
-		p = journal.PriEmerg
+		return journal.PriEmerg
 	case priority == 1:
-		p = journal.PriAlert
+		return journal.PriAlert
 	case priority == 2:
-		p = journal.PriCrit
+		return journal.PriCrit
 	case priority == 3:
-		p = journal.PriErr
+		return journal.PriErr
 	case priority == 4:
-		p = journal.PriWarning
+		return journal.PriWarning
 	case priority == 5:
-		p = journal.PriNotice
+		return journal.PriNotice
 	case priority == 6:
-		p = journal.PriInfo
+		return journal.PriInfo
 	case priority == 7:
-		p = journal.PriDebug
+		return journal.PriDebug
+	default:
+		return fallback
 	}
-
-	return p
 }
 
 func convertPriorityString(priority string, fallback journal.Priority) journal.Priority {
-	p := fallback
-
 	switch {
 	case priority == "emergency":
-		p = journal.PriEmerg
+		return journal.PriEmerg
 	case priority == "alert":
-		p = journal.PriAlert
+		return journal.PriAlert
 	case priority == "critical":
-		p = journal.PriCrit
+		return journal.PriCrit
 	case priority == "error":
-		p = journal.PriErr
+		return journal.PriErr
 	case priority == "warning":
-		p = journal.PriWarning
+		return journal.PriWarning
 	case priority == "notice":
-		p = journal.PriNotice
+		return journal.PriNotice
 	case priority == "info":
-		p = journal.PriInfo
+		return journal.PriInfo
 	case priority == "debug":
-		p = journal.PriDebug
+		return journal.PriDebug
+	default:
+		return fallback
 	}
-
-	return p
 }
