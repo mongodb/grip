e6600efe5b1137b9d6b5343c5f37526644092f4d
tycho garen <garen@tychoish.com>
2016-12-23 16:27:43 -0500

    base implementation of interfaces for slogger

---
+++ send/stream.go
@@ -0,0 +1,36 @@
+package send
+
+import "github.com/tychoish/grip/message"
+
+// this file contains tools to support the slogger interface
+
+type WriteStringer interface {
+	WriteString(str string) (int, error)
+}
+
+type streamLogger struct {
+	fobj WriteStringer
+	*base
+}
+
+func NewStreamLogger(name string, ws WriteStringer, l LevelInfo) (Sender, error) {
+	s := &streamLogger{
+		fobj: ws,
+		base: newBase(name),
+	}
+
+	if err := s.SetLevel(l); err != nil {
+		return nil, err
+	}
+	s.reset = func() {}
+
+	return s, nil
+}
+
+func (s *streamLogger) Close() error     { return nil }
+func (s *streamLogger) Type() SenderType { return Stream }
+func (s *streamLogger) Send(m message.Composer) {
+	if s.level.ShouldLog(m) {
+		_, _ = s.fobj.WriteString(m.Resolve())
+	}
+}
