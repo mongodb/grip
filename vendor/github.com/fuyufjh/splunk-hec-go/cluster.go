5a192dd369fa9c25e8c5eabb298e36e043838b51
tycho garen <garen@tychoish.com>
2017-06-10 13:02:46 -0400

    MAKE-191: make splunk package gccgo compatible

--- vendor/github.com/fuyufjh/splunk-hec-go/cluster.go
+++ vendor/github.com/fuyufjh/splunk-hec-go/cluster.go
@@ -89,9 +89,9 @@ func (c *Cluster) WriteBatch(events []*Event) error {
 }
 
 func (c *Cluster) WriteRaw(reader io.ReadSeeker, metadata *EventMetadata) error {
-	startAt, _ := reader.Seek(0, io.SeekCurrent)
+	startAt, _ := reader.Seek(0, ioSeekCurrent)
 	return c.retry(func(client *Client) error {
-		reader.Seek(startAt, io.SeekStart)
+		reader.Seek(startAt, ioSeekStart)
 		return client.WriteRaw(reader, metadata)
 	})
 }
