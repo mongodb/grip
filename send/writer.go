46627eb7393565270be21b2bcdb887c0de193d8a
Brian Samek <brian.samek@mongodb.com>
2017-09-12 17:16:01 -0400

    EVG-2067 Trim right

--- send/writer.go
+++ send/writer.go
@@ -77,11 +77,11 @@ func (s *WriterSender) doSend() error {
 		}
 
 		if err == nil {
-			s.Send(message.NewBytesMessage(s.priority, bytes.Trim(line, "\n\t ")))
+			s.Send(message.NewBytesMessage(s.priority, bytes.TrimRight(line, "\n\t ")))
 			continue
 		}
 
-		s.Send(message.NewBytesMessage(s.priority, bytes.Trim(line, "\n\t ")))
+		s.Send(message.NewBytesMessage(s.priority, bytes.TrimRight(line, "\n\t ")))
 		return err
 	}
 }
@@ -96,7 +96,7 @@ func (s *WriterSender) Close() error {
 		return err
 	}
 
-	s.Send(message.NewBytesMessage(s.priority, bytes.Trim(s.buffer.Bytes(), "\n\t ")))
+	s.Send(message.NewBytesMessage(s.priority, bytes.TrimRight(s.buffer.Bytes(), "\n\t ")))
 	s.buffer.Reset()
 	s.writer.Reset(s.buffer)
 	return nil
