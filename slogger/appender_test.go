5a50ae1553d771c57cc438c73e6aee7859a0ce63
Kim Tao <kimberly.tao@mongodb.com>
2018-09-19 11:17:41 -0400

    lint

--- slogger/appender_test.go
+++ slogger/appender_test.go
@@ -11,9 +11,9 @@ import (
 func TestDevNull(t *testing.T) {
 	devNull, err := DevNullAppender()
 	assert.NoError(t, err)
-	devNull.SetErrorHandler(func(err error, c message.Composer) {
+	assert.NoError(t, devNull.SetErrorHandler(func(err error, c message.Composer) {
 		assert.Fail(t, "Send() should not fail for DevNullAppender()")
-	})
+	}))
 
 	devNull.Send(message.NewDefaultMessage(level.Info, "foobar"))
 }
