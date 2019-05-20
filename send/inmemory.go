261706836b97d57ed3a6568cac3600231990bca7
Kim Tao <kim.tao@mongodb.com>
2019-05-20 16:20:52 -0400

    Allow in-memory logs to be read in chunks

--- send/inmemory.go
+++ send/inmemory.go
@@ -3,6 +3,7 @@ package send
 import (
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"sync"
@@ -10,13 +11,23 @@ import (
 	"github.com/mongodb/grip/message"
 )
 
+const (
+	readHeadNone      = -1
+	readHeadTruncated = -2
+)
+
+// ErrorTruncated means that the limited buffer capacity was overwritten.
+var ErrorTruncated = errors.New("buffer was truncated")
+
 // InMemorySender represents an in-memory buffered sender with a fixed message capacity.
 type InMemorySender struct {
 	Base
-	buffer         []message.Composer
-	mutex          sync.RWMutex
-	head           int
-	totalBytesSent int64
+	buffer           []message.Composer
+	mutex            sync.RWMutex
+	writeHead        int
+	readHead         int
+	readHeadCaughtUp bool
+	totalBytesSent   int64
 }
 
 // NewInMemorySender creates an in-memory buffered sender with the given capacity.
@@ -25,7 +36,11 @@ func NewInMemorySender(name string, info LevelInfo, capacity int) (Sender, error
 		return nil, errors.New("cannot have capacity <= 0")
 	}
 
-	s := &InMemorySender{Base: *NewBase(name), buffer: make([]message.Composer, 0, capacity)}
+	s := &InMemorySender{
+		Base:     *NewBase(name),
+		buffer:   make([]message.Composer, 0, capacity),
+		readHead: readHeadNone,
+	}
 	if err := s.Base.SetLevel(info); err != nil {
 		return nil, err
 	}
@@ -46,24 +61,108 @@ func NewInMemorySender(name string, info LevelInfo, capacity int) (Sender, error
 	return s, nil
 }
 
-// Get returns the messages in the buffer.
+func print(format string, args ...interface{}) {
+	fmt.Printf("kim: ")
+	fmt.Printf(format, args...)
+	fmt.Println()
+}
+
+func (s *InMemorySender) overflow() bool {
+	return len(s.buffer) == cap(s.buffer)
+}
+
+// GetCount returns at most count messages in the buffer as a stream. It returns
+// the messages and the number of messages returned. If the function is called
+// and reaches the end of the buffer, it returns io.EOF. If the position it is
+// currently reading at has been truncated, this returns ErrorTruncated. To
+// continue reading, the read stream must be reset using ResetRead.
+func (s *InMemorySender) GetCount(count int) ([]message.Composer, int, error) {
+	print("GetCount")
+	if count <= 0 {
+		return nil, 0, errors.New("cannot have count <= 0")
+	}
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if len(s.buffer) == 0 {
+		return nil, 0, io.EOF
+	}
+
+	switch s.readHead {
+	case readHeadNone:
+		if !s.overflow() {
+			s.readHead = 0
+		} else {
+			s.readHead = s.writeHead
+		}
+	case readHeadTruncated:
+		return nil, 0, ErrorTruncated
+	}
+
+	print("old read head = %d, write head = %d", s.readHead, s.writeHead)
+	remaining := s.writeHead - s.readHead
+	print("remaining = %d", remaining)
+	if remaining < 0 {
+		// write head is below read head
+		remaining += len(s.buffer)
+		print("corrected remaining (for overflow): %d", remaining)
+	} else if remaining == 0 && !s.readHeadCaughtUp {
+		// write head and read head are even but the read head has just been
+		// initialized, so it still has to read all the buffer contents before
+		// being caught up
+		print("read head and write head are even but read head was just initialized")
+		remaining = len(s.buffer)
+	}
+	if remaining < count {
+		count = remaining
+	}
+	print("count = %d", count)
+	if count == 0 {
+		return nil, 0, io.EOF
+	}
+
+	defer func() {
+		s.readHead = (s.readHead + count) % cap(s.buffer)
+		// If read head is even with write head, it is caught up.
+		s.readHeadCaughtUp = s.readHead == s.writeHead
+		print("new read head = %d, caught up = %t", s.readHead, s.readHeadCaughtUp)
+	}()
+
+	start := s.readHead
+	end := s.readHead + count
+	print("start = %d, end = %d", start, end)
+
+	if end > len(s.buffer) {
+		end = end - len(s.buffer)
+		return append(s.buffer[start:len(s.buffer)], s.buffer[0:end]...), count, nil
+	}
+	return s.buffer[start:end], count, nil
+}
+
+// ResetRead resets the read stream used in GetCount.
+func (s *InMemorySender) ResetRead() {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	s.readHead = readHeadNone
+	s.readHeadCaughtUp = false
+}
+
+// Get returns all the current messages in the buffer.
 func (s *InMemorySender) Get() []message.Composer {
 	s.mutex.RLock()
 	defer s.mutex.RUnlock()
 
-	var start int
-	var tmp []message.Composer
-
-	start = s.head - len(s.buffer)
+	start := s.writeHead - len(s.buffer)
 	if start < 0 {
 		start = start + len(s.buffer)
-		tmp = append(tmp, s.buffer[start:len(s.buffer)]...)
-		return append(tmp, s.buffer[:s.head]...)
+		tmp := append([]message.Composer{}, s.buffer[start:len(s.buffer)]...)
+		return append(tmp, s.buffer[:s.writeHead]...)
 	}
-	return append(tmp, s.buffer[start:s.head]...)
+	return s.buffer[start:s.writeHead]
 }
 
-// GetString returns the messages in the buffer as formatted strings.
+// GetString returns all the current messages in the buffer as formatted strings.
 func (s *InMemorySender) GetString() ([]string, error) {
 	msgs := s.Get()
 	strs := make([]string, 0, len(msgs))
@@ -77,7 +176,7 @@ func (s *InMemorySender) GetString() ([]string, error) {
 	return strs, nil
 }
 
-// GetRaw returns the messages in the buffer as empty interfaces.
+// GetRaw returns all the current messages in the buffer as empty interfaces.
 func (s *InMemorySender) GetRaw() []interface{} {
 	msgs := s.Get()
 	raw := make([]interface{}, 0, len(msgs))
@@ -87,22 +186,35 @@ func (s *InMemorySender) GetRaw() []interface{} {
 	return raw
 }
 
-// Send adds the given message to the buffer. If the buffer is at max capacity, it removes the oldest
-// message.
+// Send adds the given message to the buffer. If the buffer is at max capacity,
+// it truncates the oldest message.
 func (s *InMemorySender) Send(msg message.Composer) {
+	print("Send %s", msg.String())
 	if !s.Level().ShouldLog(msg) {
 		return
 	}
 
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
+	print("old write head = %d", s.writeHead)
+	if s.writeHead == s.readHead {
+		if s.readHeadCaughtUp {
+			// If read head was even with write head, it is not anymore.
+			s.readHeadCaughtUp = false
+			print("read head was caught up, but no longer caught up at position %d", s.writeHead)
+		} else {
+			s.readHead = readHeadTruncated
+			print("truncated read head at position %d", s.writeHead)
+		}
+	}
 
-	if len(s.buffer) < cap(s.buffer) {
+	if !s.overflow() {
 		s.buffer = append(s.buffer, msg)
 	} else {
-		s.buffer[s.head] = msg
+		s.buffer[s.writeHead] = msg
 	}
-	s.head = (s.head + 1) % cap(s.buffer)
+	s.writeHead = (s.writeHead + 1) % cap(s.buffer)
+	print("new write head = %d", s.writeHead)
 
 	s.totalBytesSent += int64(len(msg.String()))
 }
