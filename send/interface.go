a5082d343c8f1cf463072df3f65c664c18bdfcaf
Kim Tao <kim.tao@mongodb.com>
2019-08-23 15:59:41 -0400

    MAKE-896: add JSON tags to send.LevelInfo (#58)

--- send/interface.go
+++ send/interface.go
@@ -55,8 +55,8 @@ type Sender interface {
 // LevelInfo provides a sender-independent structure for storing
 // information about a sender's configured log levels.
 type LevelInfo struct {
-	Default   level.Priority
-	Threshold level.Priority
+	Default   level.Priority `json:"default"`
+	Threshold level.Priority `json:"threshold"`
 }
 
 // Valid checks that the priorities stored in the LevelInfo document are valid.
