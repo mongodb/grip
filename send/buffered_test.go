576817ea78f5f51ad6e7634d5dee62726733813f
julianedwards <julian.edwards@mongodb.com>
2020-01-24 15:14:36 -0500

    fix compile

--- send/buffered_test.go
+++ send/buffered_test.go
@@ -49,9 +49,9 @@ func TestBufferedSend(t *testing.T) {
 
 		bs.Send(message.ConvertToComposer(level.Debug, "should flush"))
 		time.Sleep(6 * time.Second)
-		b.mu.Lock()
+		bs.mu.Lock()
 		assert.True(t, time.Since(bs.lastFlush) <= 2*time.Second)
-		b.mu.Unlock()
+		bs.mu.Unlock()
 		msg, ok := s.GetMessageSafe()
 		require.True(t, ok)
 		assert.Equal(t, "should flush", msg.Message.String())
