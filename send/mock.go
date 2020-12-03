836688a14766a0b72e76e89a16590fa274aeb0a3
julianedwards <julian.edwards@mongodb.com>
2020-12-03 17:58:52 -0500

    documentation

--- send/mock.go
+++ send/mock.go
@@ -10,7 +10,6 @@ import (
 // MockSender is a simple mock implementation of the Sender interface.
 type MockSender struct {
 	Messages []message.Composer
-	Buffered bool
 	FlushErr bool
 	CloseErr bool
 	Closed   bool
@@ -25,10 +24,12 @@ func NewMockSender(name string) *MockSender {
 	}
 }
 
+// Send appends the message to the mock sender's messages slice.
 func (s *MockSender) Send(m message.Composer) {
 	s.Messages = append(s.Messages, m)
 }
 
+// Flush noops unless the FlushErr flag is set to true.
 func (s *MockSender) Flush(_ context.Context) error {
 	if s.FlushErr {
 		return errors.New("flush error")
@@ -37,6 +38,8 @@ func (s *MockSender) Flush(_ context.Context) error {
 	return nil
 }
 
+// Close sets the Closed flag to true. If either the CloseErr flag or the Close
+// flag are set to true, and error is returned.
 func (s *MockSender) Close() error {
 	if s.CloseErr {
 		return errors.New("close error")
