7c8d4e4e8f5798b2a07c6b3476086ff38eea4142
Kim Tao <kim.tao@mongodb.com>
2021-10-26 11:55:48 -0400

    Handle test-message in non-GOPATH way

--- message/stack_test.go
+++ message/stack_test.go
@@ -9,9 +9,14 @@ import (
 func TestPrintStack(t *testing.T) {
 	assert := assert.New(t)
 	stack := funcA()
-	assert.Contains(stack, `github.com/mongodb/grip/message/stack_test.go:26 (funcC)`)
-	assert.Contains(stack, `github.com/mongodb/grip/message/stack_test.go:22 (funcB)`)
-	assert.Contains(stack, `github.com/mongodb/grip/message/stack_test.go:18 (funcA)`)
+	// Depending on whether this is run inside of a GOPATH, it may produce
+	// different results. If grip is a subdirectory of the GOPATH, it'll include
+	// the full path to it (e.g. github.com/mongodb/grip/message/stack_test.go).
+	// However, if it's run outside of the GOPATH, it only includes the path
+	// relative to the go module root directory.
+	assert.Contains(stack, `message/stack_test.go:26 (funcC)`)
+	assert.Contains(stack, `message/stack_test.go:22 (funcB)`)
+	assert.Contains(stack, `message/stack_test.go:18 (funcA)`)
 }
 
 func funcA() string {
