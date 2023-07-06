fb5e84af12c6df7656bfe90ab482c4a18a93719c
Jonathan Brill <jonathan.brill@10gen.com>
2023-07-06 10:35:37 -0400

    things

--- send/ses.go
+++ send/ses.go
@@ -16,13 +16,13 @@ import (
 
 const maxRecipients = 50
 
-// SESOptions configures the SESLogger.
+// SESOptions configures the SES Logger.
 type SESOptions struct {
 	// Name is the name of the logger. It's also used as the friendly name in emails' From field.
 	Name string
-	// From is the default address to send emails from. Individual emails can override the from field.
-	// The From email or its domain must be verified in SES.
-	From string
+	// SenderAddress is the default address to send emails from. Individual emails can override this field.
+	// This email or its domain must be verified in SES.
+	SenderAddress string
 	// AWSConfig configures the SES client. The config must include authorization to send raw emails over SES.
 	AWSConfig aws.Config
 }
@@ -31,8 +31,8 @@ func (o *SESOptions) validate() error {
 	if o.Name == "" {
 		return errors.New("logger name must be provided")
 	}
-	if o.From == "" {
-		return errors.New("from address must be provided")
+	if o.SenderAddress == "" {
+		return errors.New("sender address must be provided")
 	}
 
 	return nil
@@ -40,15 +40,19 @@ func (o *SESOptions) validate() error {
 
 type sesSender struct {
 	options SESOptions
+	ctx     context.Context
 	Base
 }
 
-// NewSESLogger returns a configured sesSender.
-func NewSESLogger(options SESOptions, l LevelInfo) (Sender, error) {
+// NewSESLogger returns a Sender implementation backed by SES.
+func NewSESLogger(ctx context.Context, options SESOptions, l LevelInfo) (Sender, error) {
 	if err := options.validate(); err != nil {
 		return nil, errors.Wrap(err, "invalid options")
 	}
-	sender := &sesSender{options: options}
+	sender := &sesSender{
+		ctx:     ctx,
+		options: options,
+	}
 	sender.SetLevel(l)
 
 	return sender, nil
@@ -59,7 +63,7 @@ func (s *sesSender) Flush(_ context.Context) error { return nil }
 
 // Send sends the email over SES.
 func (s *sesSender) Send(m message.Composer) {
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(s.ctx)
 	defer cancel()
 
 	if !s.Level().ShouldLog(m) {
@@ -68,7 +72,7 @@ func (s *sesSender) Send(m message.Composer) {
 
 	emailMsg, ok := m.Raw().(*message.Email)
 	if !ok {
-		s.ErrorHandler()(errors.Errorf("expecting *message.Email, got '%T'", m), m)
+		s.ErrorHandler()(errors.Errorf("expecting *message.Email, got %T", m), m)
 		return
 	}
 
@@ -76,10 +80,15 @@ func (s *sesSender) Send(m message.Composer) {
 }
 
 func (s *sesSender) sendRawEmail(ctx context.Context, emailMsg *message.Email) error {
-	from := s.options.From
+	from := s.options.SenderAddress
 	if emailMsg.From != "" {
 		from = emailMsg.From
 	}
+	fromAddr, err := mail.ParseAddress(from)
+	if err != nil {
+		return errors.Wrap(err, "parsing From address")
+	}
+	fromAddr.Name = s.name
 
 	if len(emailMsg.Recipients) == 0 {
 		return errors.New("no recipients specified")
@@ -87,12 +96,6 @@ func (s *sesSender) sendRawEmail(ctx context.Context, emailMsg *message.Email) e
 	if len(emailMsg.Recipients) > maxRecipients {
 		return errors.Errorf("cannot send to more than %d recipients", maxRecipients)
 	}
-
-	fromAddr, err := mail.ParseAddress(from)
-	if err != nil {
-		return errors.Wrap(err, "parsing From address")
-	}
-	fromAddr.Name = s.name
 
 	var toAddresses []string
 	for _, address := range emailMsg.Recipients {
@@ -110,20 +113,20 @@ func (s *sesSender) sendRawEmail(ctx context.Context, emailMsg *message.Email) e
 		"MIME-Version: 1.0",
 	}
 
-	skipContentType := false
+	hasContentTypeSet := false
 	for k, v := range emailMsg.Headers {
 		if k == "To" || k == "From" || k == "Subject" || k == "Content-Transfer-Encoding" {
 			continue
 		}
 		if k == "Content-Type" {
-			skipContentType = true
+			hasContentTypeSet = true
 		}
 		for i := range v {
 			contents = append(contents, fmt.Sprintf("%s: %s", k, v[i]))
 		}
 	}
 
-	if !skipContentType {
+	if !hasContentTypeSet {
 		if emailMsg.PlainTextContents {
 			contents = append(contents, "Content-Type: text/plain; charset=\"utf-8\"")
 		} else {
